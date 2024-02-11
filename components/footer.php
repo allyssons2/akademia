5754695267651c0fbb485e89d832f0e19c7ba4f6
Allysson <allyssonchristian0@gmail.com>
2024-02-11 20:17:14 -0300

    academia

---
+++ components/footer.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="components/components.css">
+
+<footer>
+    <strong>Desenvolvido por Allysson, 2023</strong>
+    <strong>Técnico em Informática - Senac Santos</strong>
+</footer>
